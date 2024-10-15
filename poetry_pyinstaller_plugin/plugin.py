169a54ce201184c743dfe2c2511477398939bac7
Alejandro Claro <alejandro.claro@gmail.com>
2024-10-15 14:16:25 +0200

    fix: fix missing import to make it work with Python 3.8
    In order to make this work again with Python 3.8, annotations need to be imported from __future__ module

--- poetry_pyinstaller_plugin/plugin.py
+++ poetry_pyinstaller_plugin/plugin.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import annotations
+
 __author__ = "Thomas Mahé <contact@tmahe.dev>"
 
 import enum
